912b2fbb5923c45a969f9d508556b527672621f1
nelivacn <59565653+nelivacn@users.noreply.github.com>
2022-02-08 15:23:34 +0800

    Update fat_core.h
    gpu and batch

--- 1N/cxxexample/source/fat_core.h
+++ 1N/cxxexample/source/fat_core.h
@@ -15,32 +15,67 @@
 #include <fstream>
 #include "detect.h"
 #include "feature.h"
+#include "opencv2/core/cuda.hpp"
+
 
 namespace FAT {
+
+struct Image
+{
+    int nC;
+    int nW;
+    int nH;
+    char* pData;
+};
 
 class FATImpl {
     public:
         FATImpl();
-        ~FATImpl();
+        virtual ~FATImpl();
 
         static int FeatureLength() { return Feature::FeatureLength(); }
 
         void Load(const std::string& rdir);
 
         void GetFeature(char* img, int width, int height, int channel, int im_type, float* feat);
-        void Finalize(float* gallery_feats, int* gallery_labels, int N, int K);
-        void GetTopK(float* im_feat, int* topk, float* sim);
+        // int PredictMulti(const std::vector<Image> &vImage, std::vector<float> &vFeatureStream);
+        void GetFeatures(const std::vector<Image> &images, std::vector<float*> &features);
+        // void Finalize(float* gallery_feats, int* gallery_labels, int N, int K);
+        // void GetTopK(float* im_feat, int* topk, float* sim);
+        // void GetTopKs(const std::vector<float*> &im_feats, std::vector<int*> &topks, std::vector<float*> &sims);
 
 
     private:
         std::shared_ptr<Detector> detector_;
         std::shared_ptr<Feature> feature_;
-        //float* gallery_feats_;
-        cv::Mat gallery_feats_;
-        int* gallery_labels_;
-        int N_;
-        int K_;
-        std::vector<std::pair<float, int> > buffer_;
+        // float* gallery_feats_;
+        // cv::Mat gallery_feats_;
+        // cv::cuda::GpuMat gallery_feats_;
+        // int* gallery_labels_;
+        // int N_;
+        // int K_;
+        // std::vector<std::pair<float, int> > buffer_;
+
+};
+
+
+
+
+class FATGalleryQuery{
+
+public:
+    FATGalleryQuery(): gallery_labels_(NULL), N_(0) {}
+    virtual ~FATGalleryQuery(){}
+    static int FeatureLength() { return Feature::FeatureLength(); }
+    void Finalize(float* gallery_feats, int* gallery_labels, int N, int K);
+    void GetTopK(float* im_feat, int* topk, float* sim);
+    void GetTopKs(const std::vector<float*> &im_feats, std::vector<int*> &topks, std::vector<float*> &sims);
+private:
+    cv::cuda::GpuMat gallery_feats_;
+    int* gallery_labels_;
+    int N_;
+    int K_;
+    std::vector<std::pair<float, int> > buffer_;
 
 };
 
